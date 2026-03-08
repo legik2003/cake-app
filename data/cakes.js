679be4504eb6c105e82b097faab3df898ce616e1
Oleg <115182446+legik2003@users.noreply.github.com>
2026-03-09 01:28:40 +0300

    Create cakes.js

---
+++ data/cakes.js
@@ -0,0 +1,35 @@
+const cakes=[
+
+{
+name:"Шоколадный торт",
+price:45,
+category:"cake",
+img:"https://images.unsplash.com/photo-1578985545062-69928b1d9587",
+desc:"Шоколадные коржи и крем."
+},
+
+{
+name:"Клубничный торт",
+price:55,
+category:"cake",
+img:"https://images.unsplash.com/photo-1559620192-032c4bc4674e",
+desc:"Клубничная начинка."
+},
+
+{
+name:"Капкейк",
+price:5,
+category:"cupcake",
+img:"https://images.unsplash.com/photo-1486427944299-d1955d23e34d",
+desc:"Шоколадный капкейк."
+},
+
+{
+name:"Чизкейк",
+price:35,
+category:"dessert",
+img:"https://images.unsplash.com/photo-1533134242443-d4fd215305ad",
+desc:"Нежный чизкейк."
+}
+
+];
